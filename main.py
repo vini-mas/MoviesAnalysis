74bd1bbe6c973e263cfca4fdd6260b7e4435c786
Nyorok <vi.maschio@outlook.com>
2022-04-23 22:55:26 -0300

    Created ImdbDataImporter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from database_manager.database_manager import DatabaseManager
+from imdb_data_importer.imdb_data_importer import ImdbDataImporter
 
 if __name__ == "__main__":
     database_manager = DatabaseManager('root', 'admin')
@@ -6,3 +7,10 @@ if __name__ == "__main__":
     if database_manager.connection:
         database_manager.reset_database()
         database_manager.create_tables()
+    else: raise Exception('Unable to connect to database')
+    
+    imdb_data_importer = ImdbDataImporter()
+
+    print(imdb_data_importer.movies.head())
+    print(imdb_data_importer.ratings.head())
+    print(imdb_data_importer.directors_and_writers.head())
